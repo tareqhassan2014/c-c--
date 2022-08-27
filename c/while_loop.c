e888c05a7e0ff40f1a5bb230b8ece70a2a92c39f
Tareq Hasan <tareqhassan2014@gmail.com>
2022-08-27 08:37:16 +0600

    first commit

---
+++ c/while_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int money = 0;
+    int target = 100;
+
+    while (money < target)
+    {
+        printf("Need more money. Enter amount: ");
+        int given = 0;
+        scanf("%d", &given);
+        money += given;
+        printf("Current money = %d\n", money);
+    }
+
+    return 0;
+}
